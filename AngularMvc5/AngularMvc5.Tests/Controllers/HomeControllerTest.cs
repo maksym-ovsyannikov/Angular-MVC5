6be6be8a8638eb1603b3c7bacd2535b220787b04
Maksym Ovsyannikov <Maksym_Ovsyannikov@epam.com>
2018-02-13 16:10:34 +0200

    Init project

---
+++ AngularMvc5/AngularMvc5.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AngularMvc5;
+using AngularMvc5.Controllers;
+
+namespace AngularMvc5.Tests.Controllers
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual("Home Page", result.ViewBag.Title);
+        }
+    }
+}
---
+++ AngularMvc5/AngularMvc5/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace AngularMvc5.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
